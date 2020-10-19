d424aea3d2345c6f6e965a924278235f3e7a8a24
nick <nickelias57@yahoo.com>
2020-10-19 10:35:09 -0500

    Initial Commit

---
+++ finalGradeCalcSwiftUI/finalGradeCalcSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalGradeCalcSwiftUIApp.swift
+//  finalGradeCalcSwiftUI
+//
+//  Created by Nick Elias on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct finalGradeCalcSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
